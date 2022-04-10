69d6eb099de4535e53613382adf7568c6c5544cf
Sameer Al Harbi <smrhrbi@gmail.com>
2022-04-10 20:43:17 +0300

    starting typescript

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,27 @@
-import express from 'express';
-import errorsController from './controllers/errors.controller';
+import express from "express";
+import errorsController from "./controllers/errors.controller";
 
 //Express server
 const app = express();
 app.use(express.json);
 
 app.use((req, res, next) => {
-
-  req.query.lang = (req.query.lang as string).toUpperCase() || 'A';
+  req.query.lang = (req.query.lang as string).toUpperCase() || "A";
 
   next();
-
 });
 
-app.use('/500', errorsController.get404);
+app.use("/500", errorsController.get404);
 app.use(errorsController.get404);
 
 //This middleware will be called directly whene ever we call next(Error)
 app.use((error: any, req: any, res: any, next: any) => {
   error.httpStatusCode = error.httpStatusCode || 500;
-  error.message = error.httpStatusCode !== 404 ? 
-      error.message || '' : 'Data NotFound!';
-  return res.status(error.httpStatusCode).json({ error : error.message
-    , data: error.errorsData || {} });
+  error.message =
+    error.httpStatusCode !== 404 ? error.message || "" : "Data NotFound!";
+  return res
+    .status(error.httpStatusCode)
+    .json({ error: error.message, data: error.errorsData || {} });
 });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import app from './app';
+import chalk from 'chalk';
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(chalk.greenBright.inverse('started'));
+});
